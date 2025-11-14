de22aede8781ef07a5d35afd26252ea497d849bc
Zyire <zmurphy5@broncos.uncfsu.edu>
2025-11-14 10:13:46 -0500

    class_3_initialization.py finish

--- class_3_initialization.py
+++ class_3_initialization.py
@@ -3,6 +3,7 @@ class House:
     middle_school = "FV Middle"
     high_school = "FV High"
 
-    # TODO: create an initialization method, and two instance attributes:
-    # attribute name "plan" with string value "Eden"
+    def __init__(self):
+        self.plan = "Eden"
+        self.size = 2000
     # attribute name "size" with int value 2000
